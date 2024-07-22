53d0d3f7c6d5a26ec37975f7374a050e4f48293b
Alex Ishii <aishii@bu.edu>
2024-07-22 13:18:59 -0400

    work on steps 2-4

--- app.py
+++ app.py
@@ -16,6 +16,24 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', port=5004, debug=True)
+    app.run(host='0.0.0.0', port=5004, debug=True, threaded = False)
 
 @app.route('/repeat')
+def getParameter(input):
+    val = (
+        {
+            'body':input,
+            'status':200
+        }
+    )
+    return val
+
+@app.route('/health')
+def health():
+    response = make_response(
+        {
+            'body': 'OK',
+            'status': 200
+        }
+    )
+    return response
