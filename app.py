6d6a64fc1663d458cba2bc78a2af38d603918b3a
Alex Ishii <aishii@bu.edu>
2024-07-22 12:36:05 -0400

    corrected port number

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', port=5020, debug=True)
+    app.run(host='0.0.0.0', port=5004, debug=True)
+
+@app.route('/repeat')
