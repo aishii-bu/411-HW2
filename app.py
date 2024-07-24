0af8570db7f8b59e45473efd35e9f59ac1d4247f
Alex Ishii <aishii@bu.edu>
2024-07-24 12:17:42 -0400

    changed order of functions in app.py

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ def hello():
     response = make_response("Hello World", 200)
     return response
 
-if __name__ == '__main__':
-    # By default flask is only accessible from localhost.
-    # Set this to '0.0.0.0' to make it accessible from any IP address
-    # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', port=5004, debug=True, threaded = False)
-
 @app.route('/repeat')
 def getParameter(input):
     val = (input,200)
@@ -27,5 +21,11 @@ def health():
 def hang():
     # loop 4ever
     var = False
-    while var == False:{
-    }
+    while True: # var == False:
+        pass
+        
+if __name__ == '__main__':
+    # By default flask is only accessible from localhost.
+    # Set this to '0.0.0.0' to make it accessible from any IP address
+    # on your network (not recommended for production use)
+    app.run(host='0.0.0.0', port=5004, debug=True, threaded = False)
